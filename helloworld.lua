3b16fc9815cb69b221a33424f60a241529200db5
vini <velkhaliliborsato@gmail.com>
2023-08-21 00:45:24 -0300

    ../..

--- helloworld.lua
+++ helloworld.lua
@@ -1,1 +1,9 @@
-print("hello github")
+print("hello from github!\n")
+local text = "Much test"
+print (text,"\n")
+local stuff = function ()
+    local var1, var2 = "doing", "stuff"
+    print(var1..var2.."\n")
+end
+stuff()
+
