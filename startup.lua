d84cbddc4e846c0aa24557596f8252dafa2641b9
vini <velkhaliliborsato@gmail.com>
2023-08-28 22:15:24 -0300

    .

--- startup.lua
+++ startup.lua
@@ -1,10 +1,10 @@
+
 local function createFile (url,filename)
 
     term.clear()
     term.setCursorPos(1,1)
     print("starting...")
     local response = http.get(url)
-    
     if response then
         local code = response.readAll()
         fs.delete(filename)
@@ -12,8 +12,8 @@ local function createFile (url,filename)
         file.write(code)
         file.close()
     end
-
+    
 end
 
 local url1 = "https://raw.githubusercontent.com/vinicius-el-khalili/cc/master/helloworld.lua"
-createFile(url1,"helloworld.lua")
+createFile(url1,"github.lua")
