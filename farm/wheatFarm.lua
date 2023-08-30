5b8755ebadd9a9d80043f6f09201b4a372f97d97
vini <velkhaliliborsato@gmail.com>
2023-08-30 12:18:03 -0300

    .

--- farm/wheatFarm.lua
+++ farm/wheatFarm.lua
@@ -1,6 +1,9 @@
 --9x9 square
 -- move one line farming wheat
-function checkSeedState()
+
+function handleFuel() print("@handleFuel...") print("Fuel Level: " .. turtle.getFuelLevel()) local foundCoal = false; for slot = 1, 16 do local item = turtle.getItemDetail(slot); if item and item.name == "minecraft:coal" then turtle.select(slot); turtle.refuel(item.count); foundCoal = true; print("Success") print("Fuel Level: " .. turtle.getFuelLevel()) break; end; end; turtle.select(1); if not foundCoal then print("Fail") print("Couldn't find coal.") end; return foundCoal; end;
+
+function checkWheatState()
     
     success, data = turtle.inspectDown()
     if success then
